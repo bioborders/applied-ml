1b1ad2c506e8d7238c331d660640ba8de7de726c
Niro-y <52195521+Khimaera@users.noreply.github.com>
2021-02-03 16:30:07 +0000

    sklearn kNN versus kNN2

--- Classification/kNN-breast-cancer-wisconsin/kNN2.py
+++ Classification/kNN-breast-cancer-wisconsin/kNN2.py
@@ -19,45 +19,50 @@ def kNN(data, predict, k=5):
 
     kNN_votes = [i[1] for i in sorted(distances)[:k]]
     kNN_result = Counter(kNN_votes).most_common(1)[0][0]
+    confidence_kNN_result = (Counter(kNN_votes).most_common(1)[0][1] / k)
     return kNN_result
+    return confidence_kNN_result
 
+accuracies = []
 
-# load data
-df = pd.read_csv('kNN-breast-cancer-wisconsin/breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True) #replace missing data attributes
-df.drop('id', 1, inplace=True) #drop id column
-#convert all values to floats to avoid attribute being treated as a string
-full_data = df.astype(float).values.tolist()
+for i in range(99): # 100 runs
 
-# shuffle data
-random.shuffle(full_data)
+    # load data
+    df = pd.read_csv('kNN-breast-cancer-wisconsin/breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True) #replace missing data attributes
+    df.drop('id', 1, inplace=True) #drop id column
+    #convert all values to floats to avoid attribute being treated as a string
+    full_data = df.astype(float).values.tolist()
 
-# slice data
-test_size = 0.2
-train_set = {2:[], 4:[]}
-test_set = {2:[], 4:[]}
-train_data = full_data[:-int(test_size*len(full_data))] # first 80% = training data
-test_data = full_data[-int(test_size*len(full_data)):]  # last 20% = testing data
+    # shuffle data
+    random.shuffle(full_data)
 
-# populate train_set and test_set dicts
-for i in train_data:
-    train_set[i[-1]].append(i[:-1]) #last column = class (2 = benign; 4 = malignant)
+    # slice data
+    test_size = 0.2
+    train_set = {2:[], 4:[]}
+    test_set = {2:[], 4:[]}
+    train_data = full_data[:-int(test_size*len(full_data))] # first 80% = training data
+    test_data = full_data[-int(test_size*len(full_data)):]  # last 20% = testing data
 
-for i in test_data:
-    test_set[i[-1]].append(i[:-1]) #last column = class (2 = benign; 4 = malignant)
+    # populate train_set and test_set dicts
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1]) #last column = class (2 = benign; 4 = malignant)
 
-correct = 0
-total = 0
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1]) #last column = class (2 = benign; 4 = malignant)
 
-for cluster in test_set:
-    for data in test_set[cluster]: #iterate through features
-        vote = kNN(train_set, data, k=5) #sklearn's k=5 (we are comparing our kNN to this)
-        if cluster == vote:
-            correct += 1
-        total += 1
+    correct = 0
+    total = 0
 
-#result = kNN(df, new_features, k=3)
-#print(result)
+    for cluster in test_set:
+        for data in test_set[cluster]: #iterate through features
+            vote = kNN(train_set, data, k=5) #sklearn's k=5 (we are comparing our kNN to this)
+            if cluster == vote:
+                correct += 1
+            total += 1
 
-Accuracy = correct/total
-print(Accuracy)
+    accuracy = correct/total
+    #print(Accuracy)
+    accuracies.append(accuracy)
+
+print(sum(accuracies)/len(accuracies)) # mean accuracy for 100 runs
--- Classification/kNN-breast-cancer-wisconsin/sklearn_kNN.py
+++ Classification/kNN-breast-cancer-wisconsin/sklearn_kNN.py
@@ -12,29 +12,37 @@ from sklearn import preprocessing, model_selection, neighbors
 # note: cross_validation sub-module now = model_selection
 import pandas as pd
 
-# load dataset
-df = pd.read_csv('kNN-breast-cancer-wisconsin/breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True) #replace missing data attributes
-df.drop('id', 1, inplace=True) #drop id column
+accuracies = []
 
-# split into training and testing sets
-X = np.array(df.drop(['class'],1)) #features = all except class; 1 = column number
-y = np.array(df['class'])
+for i in range(99): # 100 runs
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
+    # load dataset
+    df = pd.read_csv('kNN-breast-cancer-wisconsin/breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True) #replace missing data attributes
+    df.drop('id', 1, inplace=True) #drop id column
 
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
+    # split into training and testing sets
+    X = np.array(df.drop(['class'],1)) #features = all except class; 1 = column number
+    y = np.array(df['class'])
 
-#using sklearn kNN -> 0.9785714285714285 accuracy
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(X_train, y_train)
 
-example_measures = np.array([[6, 8, 1, 3, 2, 1, 2, 3, 1]])
-#example_measures = example_measures.reshape(len(example_measures), -1)
+    accuracy = clf.score(X_test, y_test)
+    #print(accuracy)
 
-prediction = clf.predict(example_measures)
-if prediction == 2:
-    print("Benign")
-else:
-    print("Malignant")
+    #using sklearn kNN -> 0.9785714285714285 accuracy
+
+    #example_measures = np.array([[6, 8, 1, 3, 2, 1, 2, 3, 1]])
+    #example_measures = example_measures.reshape(len(example_measures), -1)
+
+    # prediction = clf.predict(example_measures)
+    # if prediction == 2:
+    #     print("Benign")
+    # else:
+    #     print("Malignant")
+
+    accuracies.append(accuracy)
+
+print(sum(accuracies)/len(accuracies)) # mean accuracy for 100 runs
