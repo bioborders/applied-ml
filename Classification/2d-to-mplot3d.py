2fba4b236a6fd04b71344f1d506c67ef62ca5a19
Niro-y <52195521+Khimaera@users.noreply.github.com>
2021-02-09 20:58:07 +0000

    kernels-svm

--- Classification/2d-to-mplot3d.py
+++ Classification/2d-to-mplot3d.py
@@ -4,17 +4,17 @@ from matplotlib import style
 import numpy as np
 import math
 
-style.use('ggplot')
+style.use('gadfly')
 
 def transformation(x1, x2):
     return x1**2, 2*x2**2, x2
 
 c1 = np.array([[1,1],
-               [1,1],
-               [1,1],
-               [1,1],
-               [1,1],
-               [1,1],
+               [3,1],
+               [2,1],
+               [2,0],
+               [3,6],
+               [-5,4],
                [1,1]])
 
 c2 = np.array([[2,5],
@@ -27,9 +27,9 @@ fig = plt.figure()
 ax1 = fig.add_subplot(121)
 ax2 = fig.add_subplot(122, projection='3d')
 
-ax1.scatter(c1[:,0], c1[:,1], color='r', marker='*', s=200)
-ax1.scatter(c2[:,0], c2[:,1], color='b', marker='*', s=200)
-ax2.scatter(c1_3d[:,0], c1_3d[:,1], c1_3d[:,2], c='r', marker='*', s=200)
-ax2.scatter(c2_3d[:,0], c2_3d[:,1], c2_3d[:,2], c='b', marker='*', s=200)
+ax1.scatter(c1[:,0], c1[:,1], marker='.', s=100)
+ax1.scatter(c2[:,0], c2[:,1], marker='.', s=100)
+ax2.scatter(c1_3d[:,0], c1_3d[:,1], c1_3d[:,2], marker='.', s=100)
+ax2.scatter(c2_3d[:,0], c2_3d[:,1], c2_3d[:,2], marker='.', s=100)
 
-plt,show()
+plt.show()
---
+++ Classification/SVM-breast-cancer-classification/kernels-svm.py
@@ -0,0 +1,16 @@
+# SVM very cumbersome to train due to quadratic programming and convex optimisation problem
+# kernels are a similarity f(x)
+
+# kernels make working with non-linear data easier by transforming the feature set
+# into a different dimension and creating a linearly-seperable context without the processing cost
+
+# inner product and dot product effectively the same (np.dot versus np.inner)
+
+# recall that y = +/-((w-> . x->) + b)
+# transforming a given dimension x, to any other dimension will not change the classifier
+
+# y_i(x_i*w + b) - 1 >= 0
+# W = sum(a_i * y_i * x_i)
+# L = sum(a_i - 1/2 sum(a_i * a_j*y_i*y_j).(x_i . x_j))
+
+# POLYNOMIAL KERNAL
